0ffb450e89e2c247808466b7998b3490924dbba8
Nishant <ndjangra1027@gmail.com>
2023-03-07 22:53:57 +0530

    Added /work

--- app.js
+++ app.js
@@ -12,36 +12,57 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
 
 
 var items = [];
+var workItems = [];
 
+
+// Routing for home page
 app.get('/', (req, res) => {
     let today = new Date();
-    let options = { weekday: 'long', day : 'numeric', month: 'long'}
-    let day = today.toLocaleString('en-US',options);
+    let options = { weekday: 'long', day: 'numeric', month: 'long' }
+    let day = today.toLocaleString('en-US', options);
 
     res.render('list', {
-        kindOfDay: day,
-        newListItem : items
+        listTitle: day,
+        newListItem: items
     });
 })
 
 app.post('/', (req, res) => {
     let item = req.body.newItem;
-    if(item == ""){
+    if (item == "") {
         console.log("Empty item");
     }
     else items.push(item);
-
     res.redirect('/');
 })
 
 
+// Routing for work page
+app.get('/work', (req, res) => {
+    res.render('list', {
+        listTitle: "Work",
+        newListItem: workItems
+    });
+})
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItem;
+    if (item == "") {
+        console.log("Empty item");
+    }
+    else workItems.push(item);
+
+    res.redirect('/work');
+})
+
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`)
+    console.log(`Server is running at http://localhost:${port}`)
 })
