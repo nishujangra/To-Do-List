69c90e4597ad4bd1d5b6f62382970b24d3a4f5cf
Nishant <ndjangra1027@gmail.com>
2023-04-15 02:45:01 +0530

    Work list deleted and Database added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const date = require(__dirname + '/date.js')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://nishujangra27:4cdd0509fxnx@todo.awuyguz.mongodb.net/?retryWrites=true&w=majority')
 
 
 const app = express()
@@ -18,47 +20,45 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Set EJS as templating engine
 app.set('view engine', 'ejs');
 
+const itemsSchema = {
+    name: String
+}
 
-const items = [];
-const workItems = [];
+const items = mongoose.model('items', itemsSchema);
 
 
 // Routing for home page
 app.get('/', (req, res) => {
-    let day = date.getDate();
-    res.render('list', {
-        listTitle: day,
-        newListItem: items
-    });
+    items.find().then((itemlist) => {
+        res.render('list', {
+            listTitle: "Today",
+            newListItem: itemlist
+        });
+    })
 })
 
+
 app.post('/', (req, res) => {
-    let item = req.body.newItem;
-    if (item == "") {
+    const item = new items({
+        name: req.body.newItem
+    })
+    if (item.name == "") {
         console.log("Empty item");
     }
-    else items.push(item);
+    else item.save();
     res.redirect('/');
 })
 
 
-// Routing for work page
-app.get('/work', (req, res) => {
-    res.render('list', {
-        listTitle: "Work",
-        newListItem: workItems
-    });
+app.post('/delete', (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    items.findByIdAndRemove(checkedItemId).then((err) => {
+        if (err) console.log(err);
+        else console.log("Deleted");
+    })
+    res.redirect('/');
 })
 
-app.post('/work', (req, res) => {
-    let item = req.body.newItem;
-    if (item == "") {
-        console.log("Empty item");
-    }
-    else workItems.push(item);
-
-    res.redirect('/work');
-})
 
 
 app.listen(port, () => {
